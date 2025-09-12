030242eb64d3a779da1054eec15abfa7cbf9f328
unknown <akeem.mighty@gmail.com>
2025-09-12 12:42:20 -0400

    Completed Assigning New Pointer A Variable Address + Completed Updating Variable With New Value Using Pointer Logic

--- ex2.c
+++ ex2.c
@@ -2,10 +2,11 @@
 
 int main() {
     int a = 10;
-
+    int *ptr = &a;
     // TODO: Create a pointer variable and initialize it with the address of a.
     // TODO: Change the value of `a` using the pointer variable.
 
+    *ptr = 50;
     printf("a = %d\n", a);
 
     return 0;
